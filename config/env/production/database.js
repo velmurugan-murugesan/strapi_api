653f63d764d946d7af63ac610396b406207fec91
velmurugan.murugesan <velmurugan.murugesan@gmail.com>
2023-10-11 23:07:03 +0530

    updated db

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,19 +1,16 @@
 module.exports = ({ env }) => ({
-    defaultConnection: "default",
-    connections: {
-      default: {
-        connector: "bookshelf",
-        settings: {
-          client: "postgres",
-          host: env("DATABASE_HOST", "localhost"),
-          port: env.int("DATABASE_PORT", 3306),
-          database: env("DATABASE_NAME", "strapi"),
-          username: env("DATABASE_USERNAME", "strapi"),
-          password: env("DATABASE_PASSWORD", "strapi"),
-          ssl: {
-            ca: env("DATABASE_CA", ""),
-          },
+    connection: {
+      client: 'mysql',
+      connection: {
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'strapi'),
+        user: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+        ssl: {
+          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
         },
       },
+      debug: false,
     },
   });
