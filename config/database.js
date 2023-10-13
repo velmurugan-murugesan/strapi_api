bd417c109daf838385e2f68e5230d33456190bdc
velmurugan.murugesan <velmurugan.murugesan@gmail.com>
2023-10-13 21:23:31 +0530

    updated db

--- config/database.js
+++ config/database.js
@@ -18,10 +18,7 @@ module.exports = ({ env }) => {
           ca: env('DATABASE_SSL_CA', undefined),
           capath: env('DATABASE_SSL_CAPATH', undefined),
           cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool(
-            'DATABASE_SSL_REJECT_UNAUTHORIZED',
-            true
-          ),
+          rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
         },
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
